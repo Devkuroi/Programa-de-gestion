66fcbaf648df336e3ba9ac4dc9935057c59fcc08
Devkuroi <joan2000oviedo@gmail.com>
2025-09-17 09:32:07 -0500

    se cambio a un proyecto spring

---
+++ src/main/java/com/programagestion/ProgramaGestionApplication.java
@@ -0,0 +1,13 @@
+package com.programagestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaGestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramaGestionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/programagestion/ProgramaGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programagestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramaGestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
